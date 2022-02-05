f5ac8c65a2cc66bb9a86c3472f20729b6194eabc
Osamah <osamah.bukraa@twenty5.be>
2022-02-05 14:45:04 +0100

    Add backend watch script, add web3auth package

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+// import { Web3Auth } from "@web3auth/web3auth";
+// import { CHAIN_NAMESPACES, CustomChainConfig } from "@web3auth/base";
+
+// const polygonMumbaiConfig: CustomChainConfig = {
+//   chainNamespace: CHAIN_NAMESPACES.EIP155,
+//   rpcTarget: "https://rpc-mumbai.maticvigil.com",
+//   blockExplorer: "https://mumbai-explorer.matic.today",
+//   chainId: "0x13881",
+//   displayName: "Polygon Mumbai Testnet",
+//   ticker: "matic",
+//   tickerName: "matic",
+// };
+
+// const web3auth = new Web3Auth({
+//     chainConfig: polygonMumbaiConfig,
+//     clientId: "BKPxkCtfC9gZ5dj-eg-W6yb5Xfr3XkxHuGZl2o2Bn8gKQ7UYike9Dh6c-_LaXlUN77x0cBoPwcSx-IVm0llVsLA"
+// });
+
+
 
 function App() {
   return (
@@ -11,7 +30,9 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          // onClick={async () => {
+          //   const provider = await web3auth.connect();
+          // }}
           target="_blank"
           rel="noopener noreferrer"
         >
