5f455ec3e6f26e4dbf097b21f30b79453b49ab20
Osamah <osamah.bukraa@twenty5.be>
2022-02-05 21:44:56 +0100

    Take web3auth object out of the React App, so we always use the same instance

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,21 @@ import { useEffect, useState } from 'react';
 import { Login } from './pages/Login';
 import { Dashboard } from './pages/Dashboard';
 
+const polygonMumbaiConfig = {
+  chainNamespace: CHAIN_NAMESPACES.EIP155,
+  rpcTarget: "https://rpc-mumbai.maticvigil.com",
+  blockExplorer: "https://mumbai-explorer.matic.today",
+  chainId: "0x13881",
+  displayName: "Polygon Mumbai Testnet",
+  ticker: "matic",
+  tickerName: "matic",
+};
+
+const web3auth = new Web3Auth({
+  chainConfig: polygonMumbaiConfig,
+  clientId: "BKPxkCtfC9gZ5dj-eg-W6yb5Xfr3XkxHuGZl2o2Bn8gKQ7UYike9Dh6c-_LaXlUN77x0cBoPwcSx-IVm0llVsLA"
+});
+
 function RillApp() {
   const [user, setUser] = useState(null);
   const [loaded, setLoaded] = useState(false);
@@ -41,21 +56,6 @@ function RillApp() {
 
     setSubscribed(true);
   };
-  
-  const polygonMumbaiConfig = {
-    chainNamespace: CHAIN_NAMESPACES.EIP155,
-    rpcTarget: "https://rpc-mumbai.maticvigil.com",
-    blockExplorer: "https://mumbai-explorer.matic.today",
-    chainId: "0x13881",
-    displayName: "Polygon Mumbai Testnet",
-    ticker: "matic",
-    tickerName: "matic",
-  };
-
-  const web3auth = new Web3Auth({
-    chainConfig: polygonMumbaiConfig,
-    clientId: "BKPxkCtfC9gZ5dj-eg-W6yb5Xfr3XkxHuGZl2o2Bn8gKQ7UYike9Dh6c-_LaXlUN77x0cBoPwcSx-IVm0llVsLA"
-  });
 
   // ⭐️ initialize modal on page mount.
   const initializeModal = async () => {
